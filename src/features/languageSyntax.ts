9b7f3a75436dac9c2899d3660a395f459cd1f14b
NeftaliGC <aliguiacruz@outlook.com>
2025-06-27 23:18:36 -0600

    chore: reorganización de estructura de archivos y carpetas

--- src/features/languageSyntax.ts
+++ src/features/languageSyntax.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
 import { getCodeIdentifier } from './codeIdentifier';
-import { updateStatusBar } from './statusBarItem';
+import { updateStatusBar } from '../core/statusBarItem';
 
 interface languageSyntax {
 	lineComment?: string;
